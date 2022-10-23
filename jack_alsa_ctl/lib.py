cc1ab1442524ff4b5de6dc651b73fd71894ed801
DCsunset <DCsunset@users.noreply.github.com>
2022-10-23 14:12:29 -0400

    chore: fix indentation

--- jack_alsa_ctl/lib.py
+++ jack_alsa_ctl/lib.py
@@ -19,99 +19,99 @@ import re
 # List all sound cards
 # max_num <= 0 means no limit
 def list_cards(max_num: int = 0) -> list[str]:
-    res = subprocess.run(["cat", "/proc/asound/cards"], capture_output=True)
-    if res.returncode != 0:
-        return []
-    out = res.stdout.decode("utf-8")
-    regex = re.compile(r"\[\s*(.+?)\s*\]")
-    cards = regex.findall(out)
-    if max_num > 0:
-        n = min(max_num, len(cards))
-        # prioritize cards from the end
-        # because they are external
-        cards = cards[-n:]
-    return cards
+	res = subprocess.run(["cat", "/proc/asound/cards"], capture_output=True)
+	if res.returncode != 0:
+		return []
+	out = res.stdout.decode("utf-8")
+	regex = re.compile(r"\[\s*(.+?)\s*\]")
+	cards = regex.findall(out)
+	if max_num > 0:
+		n = min(max_num, len(cards))
+		# prioritize cards from the end
+		# because they are external
+		cards = cards[-n:]
+	return cards
    
 
 # Get current jack2 device using jack_control
 def get_jack_device():
-    try:
-        res = subprocess.run(["jack_control", "dp"], capture_output=True)
-        if res.returncode != 0:
-            return "0"
-        lines = res.stdout.decode("utf-8").splitlines()
-        for line in lines:
-            l = line.strip()
-            if l.startswith("device:"):
-                # l[:-1] removes the last parathesis
-                return l[:-1].split(":")[-1]
-    except Exception as e:
-        print(e, file=sys.stderr)
-    # return default device on failure
-    return "0"
+	try:
+		res = subprocess.run(["jack_control", "dp"], capture_output=True)
+		if res.returncode != 0:
+			return "0"
+		lines = res.stdout.decode("utf-8").splitlines()
+		for line in lines:
+			l = line.strip()
+			if l.startswith("device:"):
+				# l[:-1] removes the last parathesis
+				return l[:-1].split(":")[-1]
+	except Exception as e:
+		print(e, file=sys.stderr)
+	# return default device on failure
+	return "0"
 
 # Set/change current jack device
 def set_jack_device_cmd(dev: str) -> list[str]:
-    return [
-        f"jack_control dps device hw:{dev}",
-        # Restart jack after setting dev
-        "jack_control stop",
-        "jack_control start"
-    ]
+	return [
+		f"jack_control dps device hw:{dev}",
+		# Restart jack after setting dev
+		"jack_control stop",
+		"jack_control start"
+	]
 
 # Get scontrols of a sound card
 def get_amixer_scontrols(card: str) -> list[str]:
-    try:
-        res = subprocess.run(["amixer", "-c", card, "scontrols"], capture_output=True)
-        if res.returncode != 0:
-            return []
-            
-        names = []
-        lines = res.stdout.decode("utf-8").splitlines()
-        p = re.compile(r"'(.+)'")
-        for l in lines:
-            # find the last occurrence of the quoted name
-            name = p.search(l).groups()[-1]
-            names.append(name)
-        return names
-    except Exception as e:
-        print(e, file=sys.stderr)
+	try:
+		res = subprocess.run(["amixer", "-c", card, "scontrols"], capture_output=True)
+		if res.returncode != 0:
+			return []
+			
+		names = []
+		lines = res.stdout.decode("utf-8").splitlines()
+		p = re.compile(r"'(.+)'")
+		for l in lines:
+			# find the last occurrence of the quoted name
+			name = p.search(l).groups()[-1]
+			names.append(name)
+		return names
+	except Exception as e:
+		print(e, file=sys.stderr)
 
-    # return empty on failure
-    return []
+	# return empty on failure
+	return []
 
-    
+	
 # Get current volume scontrol and card for amixer
 def get_current_control() -> tuple[str, str]:
-    card = get_jack_device()
-    scontrols = get_amixer_scontrols(card)
+	card = get_jack_device()
+	scontrols = get_amixer_scontrols(card)
 
-    # possible scontrols for volume
-    amixer_volume_scontrols = ["Master", "Headset"]
-    for sc in amixer_volume_scontrols:
-        if sc in scontrols:
-            return card, sc
-    
-    print(f"No suitable amixer volume control for card {card}", file=sys.stderr)
-    return card, ""
+	# possible scontrols for volume
+	amixer_volume_scontrols = ["Master", "Headset"]
+	for sc in amixer_volume_scontrols:
+		if sc in scontrols:
+			return card, sc
+	
+	print(f"No suitable amixer volume control for card {card}", file=sys.stderr)
+	return card, ""
 
 def get_volume_cmd() -> str:
-    card, scontrol = get_current_control()
-    return f"amixer -c '{card}' sget '{scontrol}'"
+	card, scontrol = get_current_control()
+	return f"amixer -c '{card}' sget '{scontrol}'"
 
 def mute_cmd() -> str:
-    card, scontrol = get_current_control()
-    return f"amixer -c '{card}' sset '{scontrol}' toggle"
+	card, scontrol = get_current_control()
+	return f"amixer -c '{card}' sset '{scontrol}' toggle"
 
 def mic_mute_cmd() -> str:
-    card = get_jack_device()
-    return f"amixer -c '{card}' sset Capture toggle"
+	card = get_jack_device()
+	return f"amixer -c '{card}' sset Capture toggle"
 
 def lower_volume_cmd(step: int) -> str:
-    card, scontrol = get_current_control()
-    return f"amixer -c '{card}' sset '{scontrol}' {step}-"
+	card, scontrol = get_current_control()
+	return f"amixer -c '{card}' sset '{scontrol}' {step}-"
 
 def raise_volume_cmd(step: int) -> str:
-    card, scontrol = get_current_control()
-    return f"amixer -c '{card}' sset '{scontrol}' {step}+"
+	card, scontrol = get_current_control()
+	return f"amixer -c '{card}' sset '{scontrol}' {step}+"
 
