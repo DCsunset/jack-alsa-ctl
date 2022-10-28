a1a12028778acdfaeb7e298028b5c73a0c8dee39
DCsunset <DCsunset@users.noreply.github.com>
2022-10-27 22:51:55 -0400

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+# jack-alsa-ctl
+# Copyright (C) 2022 DCsunset
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+from pathlib import Path
+
+version = "0.1.0"
+
+repo_dir = Path(__file__).parent.absolute()
+
+# Long description
+readme = repo_dir.joinpath('README.md')
+with open(readme, "r") as f:
+	long_description = f.read()
+
+setup(
+	name="jack-alsa-ctl",
+	version=version,
+	description="Control JACK audio with ALSA driver easily",
+	long_description=long_description,
+	author="DCsunset",
+	author_email='DCsunset@protonmail.com',
+	license="AGPL-3.0",
+	url="https://github.com/DCsunset/jack-alsa-ctl",
+	packages=["jack_alsa_ctl"],
+	scripts=["bin/jack-alsa-ctl"],
+	classifiers=[
+		"Environment :: Console",
+		"Topic :: Multimedia :: Sound/Audio",
+		'Intended Audience :: End Users/Desktop',
+		'Programming Language :: Python',
+		"License :: OSI Approved :: GNU Affero General Public License v3"
+	]
+)
